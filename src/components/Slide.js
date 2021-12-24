24cb968bb5c02f7b68f5034c9b70fdf5bc1fc699
Mohamed <mohameddrame907@gmail.com>
2021-12-24 21:56:59 +0100

    ajout des liens pour chaque image du slide

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -6,11 +6,27 @@ import 'react-slideshow-image/dist/styles.css'
 import '../css/slide.css'
 
  
+let url = (indeximg) => {
+
+  if(indeximg == 0){
+    return '#'
+  }else if(indeximg == 1) {
+    return 'https://www.youtube.com/'
+  }else if(indeximg == 2){
+    return '#'
+  }else if(indeximg == 3){
+    return '#'
+  }else if(indeximg == 4){
+    return '#'
+  }
+
+}
 
 const Slideshow = () => {
     const images = [
         'accueilquiz.png',
         'Portfolio.png',
+        'JavaFx.png',
         'pfs.png',
         'Formulaire.png'
       ];
@@ -19,7 +35,7 @@ const Slideshow = () => {
       <div className="slide-container">
         <Zoom scale={0.4}>
           {
-            images.map((each, index) => <img key={index} style={{width: "100%"}} src={each} />)
+            images.map((each, index) => <a href={url(index)}><img key={index} style={{width: "100%"}} src={each} /> </a>)
           }
         </Zoom>
       </div>
