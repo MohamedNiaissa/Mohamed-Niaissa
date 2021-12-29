47a3c415aa50a75cdf310a6f181977750f2c2714
Mohamed <mohameddrame907@gmail.com>
2021-12-29 16:06:37 +0100

    reglage bug des div agencement bizarre en cours

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,34 @@ class Pageweb extends React.Component{
             <>
 
                 <Header />
-                <Presentation />
+                <div className="presentation">
+                <div className="imgmoi">
+                    <img src="moi1.jpeg" className="moi"alt="image de Mohamed" />
+                </div>
+        
+                <div className="pres">
+                    <p>
+                        Je suis Mohamed Niaissa,
+                        <br />
+                        <br />
+                        un apprenti analyste et développeur web passioné par le monde du web et mobile depuis très jeune. Mon but est de devenir un expert dans ces domaines et viser le métier de développeur full-stack
+                    </p>
+
+                    <ul className="listeResume">
+                        <li className="nom">Nom: Mohamed Niaissa</li>
+                        <li className="age">Age: 20 ans</li>
+                        <li className="ville">Ville: Clichy-La-Garenne</li>
+                        
+                    </ul>
+                
+                    <a href="#"><img src="download.png" className = "download"alt="Telecharger mon CV" /></a>
+                     
+        
+                    
+                        
+                </div>
+            
+            </div>
                 <Formation />
                 <Competances />
                 <Realisation />
